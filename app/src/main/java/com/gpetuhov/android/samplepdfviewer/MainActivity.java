9701b9d18f23b4231621c08e49fa48e3c634985a
Gennadiy Petuhov <g3pmail@gmail.com>
2017-04-10 22:19:33 +0400

    Add code

--- app/src/main/java/com/gpetuhov/android/samplepdfviewer/MainActivity.java
+++ app/src/main/java/com/gpetuhov/android/samplepdfviewer/MainActivity.java
@@ -1,13 +1,26 @@
 package com.gpetuhov.android.samplepdfviewer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.barteksc.pdfviewer.PDFView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String SAMPLE_PDF = "sample.pdf";
+    private PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pdfView = (PDFView) findViewById(R.id.pdfView);
+
+        pdfView.fromAsset(SAMPLE_PDF)
+                .enableSwipe(true)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .load();
     }
 }
